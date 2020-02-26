3ab46b1e93e55d5eedeae7b75e25671b23bbb906
Patrik Johnson <patrik.johnson@wunderdog.fi>
2020-02-26 21:48:11 +0200

    Add compiled resources, NB. this is temporary pending PR and upstream release

---
+++ dist/types/index.d.ts
@@ -0,0 +1,15 @@
+export {
+    ErrorHook,
+    JsRouterState,
+    Route,
+    RouterState,
+    RouterStore,
+    StringMap,
+    TransitionHook
+} from './router-store';
+export { ViewMap, RouterViewProps, RouterView } from './components/router-view';
+export { Link, LinkProps } from './components/link';
+export { RouterLink, RouterLinkProps } from './components/router-link';
+export { HistoryAdapter } from './adapters/history-adapter';
+export { StaticAdapter } from './adapters/static-adapter';
+export { generateUrl, routerStateToUrl } from './adapters/generate-url';
